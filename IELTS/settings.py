01d87c045f7ec023d0ad09ff6dcb554065ed680a
Pavan-Red-E <38348526+Pavan-Red-E@users.noreply.github.com>
2019-06-29 21:50:42 +0530

    Update settings.py

--- IELTS/settings.py
+++ IELTS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')pu9kg^8sowvvlsxz4lx0(!sqo62wm^1_6e8-0yei33h$bpd5^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.aiguru.ninja']
 
 
 # Application definition
